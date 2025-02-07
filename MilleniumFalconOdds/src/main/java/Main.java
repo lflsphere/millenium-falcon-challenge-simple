3f0b8a8c65be01dcd71b84d8f984260aeef7e19b
Louis FARCOT LAFOND <louis.farcot_lafond@telecom-sudparis.eu>
2025-02-07 17:04:41 +0100

    Tests run in Main

--- MilleniumFalconOdds/src/main/java/Main.java
+++ MilleniumFalconOdds/src/main/java/Main.java
@@ -13,12 +13,24 @@ public class Main {
 		C3PO c3PO = new C3PO(new File("src/main/java/examples/example1/millennium-falcon.json"));
 		//C3PO c3PO = new C3PO(new File("millenium-falcon.json"));
 		
-		double odds;
+		
 		try {
-			odds = c3PO.giveMeTheOdds(new File("src/main/java/examples/example3/empire.json"));
-			
-			System.out.println(odds);
+			double odds1;
+			odds1 = c3PO.giveMeTheOdds(new File("src/main/java/examples/example1/empire.json"));
+			System.out.println("Example1: " + odds1);
 
+			double odds2;
+			odds2 = c3PO.giveMeTheOdds(new File("src/main/java/examples/example2/empire.json"));
+			System.out.println("Example2: " + odds2);
+			
+			double odds3;
+			odds3 = c3PO.giveMeTheOdds(new File("src/main/java/examples/example3/empire.json"));
+			System.out.println("Example3: " + odds3);
+			
+			double odds4;
+			odds4 = c3PO.giveMeTheOdds(new File("src/main/java/examples/example4/empire.json"));
+			System.out.println("Example4: " + odds4);
+			
 			
 		} catch (StreamReadException e) {
 			// TODO Auto-generated catch block
